f8a850214155335480b0a1b827b09c2860bd9fa1
gingerab <singleplayer176@gmail.com>
2024-11-09 00:11:54 +0100

    siski

--- piska.py
+++ piska.py
@@ -1,14 +1,12 @@
-import subprocess
 import os
 from tkinter import *
 from tkinter import ttk
 
 def zopa():
-    subprocess.run(["chmod +x piska", "-l"])
     i=10
     while (i>1):
-         subprocess.run(["./piska", "-l"])
-         i=i-1
+        os.system('start pipiska.exe')
+        i=i-1
 
 root = Tk()
 frm = ttk.Frame(root, padding=10)
