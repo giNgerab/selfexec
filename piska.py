0093a800fb95d4160ff1c12bb53c5cd5fdbb0784
gingerab <singleplayer176@gmail.com>
2024-11-08 23:33:08 +0100

    a

---
+++ piska.py
@@ -0,0 +1,16 @@
+import subprocess
+from tkinter import *
+from tkinter import ttk
+
+def zopa():
+    i=10
+    while (i>1):
+         subprocess.run(["./piska", "-l"])
+         i=i-1
+
+root = Tk()
+frm = ttk.Frame(root, padding=10)
+frm.grid()
+ttk.Label(frm, text="Hello World!").grid(column=0, row=0)
+ttk.Button(frm, text="Quit", command=zopa).grid(column=1, row=0)
+root.mainloop()
